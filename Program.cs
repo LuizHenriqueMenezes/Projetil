5cb593d2b4654b4752bc67e52685dce73fcd4808
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-16 12:42:22 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Projétil --");
+
+            Console.WriteLine("Entre com a velocidade, em m/s:");
+            double velo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entre com o ângulo, em graus:");
+            double ang = Convert.ToDouble(Console.ReadLine());
+
+            double pi = Math.PI;
+            double g = 9.80665;
+            
+            double seno = Math.Sin(pi * (ang/180));
+            double senoAng = Math.Sin(pi * (2 * ang/180));
+
+            double alcance = (Math.Pow(velo, 2) * senoAng) / g;
+            double altura = Math.Pow(velo * senoAng, 2) / (2 * g);
+            
+            Console.WriteLine($"Alcance: {alcance:N2} m");
+            Console.WriteLine($"Altura máxima: {altura:N2} m");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Projetil
+
 Um projétil é lançado em um ângulo θ° (teta graus) a uma velocidade inicial v0 m/s. Calcule, em metros, o alcance máximo xmax e a altura máxima atingida hmax.
+
+```cs
+-- Projétil --
+
+Entre com a velocidade, em m/s..: 30
+Entre com o ângulo, em graus....: 30
+
+Alcance........: 79,48 m
+Altura máxima..: 11,47 m
+```
